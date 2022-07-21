f26e2909cec72391fa805b88a04ee19c0416761f
benjamin.frey <benjamin.frey@IC-CS233-25.uc.ucalgary.ca>
2022-07-21 16:55:57 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Ben's Label"));
+			root.getChildren().add(new Label("Ben's Branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Brendan's Project");
